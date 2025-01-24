678ba0952c239b8fb73ee57f2310225aecf01e92
DiambarSENE <d.sene20160636@zig.univ.sn>
2025-01-24 11:36:38 +0100

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/gestionEnseignement/gestionEnseignement/GestionEnseignementApplication.java
@@ -0,0 +1,13 @@
+package com.gestionEnseignement.gestionEnseignement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEnseignementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEnseignementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionEnseignement/gestionEnseignement/GestionEnseignementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionEnseignement.gestionEnseignement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEnseignementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
